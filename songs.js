f8e99a7d8a59b4b3743907403b1f87c9dd4ea2f3
sahu97 <159182432+sahu97@users.noreply.github.com>
2025-11-10 23:03:04 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,346 @@
+import song1 from "../src/assets/song1.mp3"
+import song2 from "../src/assets/song2.mp3"
+import song3 from "../src/assets/song3.mp3"
+import song4 from "../src/assets/song4.mp3"
+import song5 from "../src/assets/song5.mp3"
+import song6 from "../src/assets/song6.mp3"
+import song7 from "../src/assets/song7.mp3"
+import song8 from "../src/assets/song8.mp3"
+import song9 from "../src/assets/song9.mp3"
+import song10 from "../src/assets/song10.mp3"
+import song11 from "../src/assets/song11.mp3"
+import song12 from "../src/assets/song12.mp3"
+import song13 from "../src/assets/song13.mp3"
+import song14 from "../src/assets/song14.mp3"
+import song15 from "../src/assets/song15.mp3"
+import song16 from "../src/assets/song16.mp3"
+import song17 from "../src/assets/song17.mp3"
+import song18 from "../src/assets/song18.mp3"
+import song19 from "../src/assets/song19.mp3"
+import song20 from "../src/assets/song20.mp3"
+import song21 from "../src/assets/song21.mp3"
+import song22 from "../src/assets/song22.mp3"
+import song23 from "../src/assets/song23.mp3"
+import song24 from "../src/assets/song24.mp3"
+import song25 from "../src/assets/song25.mp3"
+import song26 from "../src/assets/song26.mp3"
+import song27 from "../src/assets/song27.mp3"
+import song28 from "../src/assets/song28.mp3"
+import song29 from "../src/assets/song29.mp3"
+import song30 from "../src/assets/song30.mp3"
+import song31 from "../src/assets/song31.mp3"
+import image1 from "../src/assets/image1.jpg"
+import image2 from "../src/assets/image2.jpg"
+import image3 from "../src/assets/image3.jpg"
+import image4 from "../src/assets/image4.jpg"
+import image5 from "../src/assets/image5.jpg"
+import image6 from "../src/assets/image6.jpg"
+import image7 from "../src/assets/image7.jpg"
+import image8 from "../src/assets/image8.jpg"
+import image9 from "../src/assets/image9.jpg"
+import image10 from "../src/assets/image10.jpg"
+import image11 from "../src/assets/image11.jpg"
+import image12 from "../src/assets/image12.jpg"
+import image13 from "../src/assets/image13.jpg"
+import image14 from "../src/assets/image14.jpg"
+import image15 from "../src/assets/image15.jpg"
+import image16 from "../src/assets/image16.jpg"
+import image17 from "../src/assets/image17.jpg"
+import image18 from "../src/assets/image18.jpg"
+import image19 from "../src/assets/image19.jpg"
+import image20 from "../src/assets/image20.jpg"
+import image21 from "../src/assets/image21.jpg"
+import image22 from "../src/assets/image22.jpg"
+import image23 from "../src/assets/image23.jpg"
+import image24 from "../src/assets/image24.jpg"
+import image25 from "../src/assets/image25.jpg"
+import image26 from "../src/assets/image26.jpg"
+import image27 from "../src/assets/image27.jpg"
+import image28 from "../src/assets/image28.jpg"
+import image29 from "../src/assets/image29.jpg"
+import image30 from "../src/assets/image30.jpg"
+import image31 from "../src/assets/image31.jpg"
+
+export const songsData = [
+  {
+    id: 1,
+    name: "Tum Ho Toh",
+    song: song1,
+    image: image1,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Vishal Mishra, Hansika Pareek"
+  },
+  {
+    id: 2,
+    name: "Kun Faya Kun",
+    song: song2,
+    image: image2,
+    category: "Romantic, Emotion",
+    liked: false,
+    singer: "A.R. Rahman, Javed Ali, Mohit Chauhan"
+  },
+  {
+    id: 3,
+    name: "Sahiba",
+    song: song3,
+    image: image3,
+    category: "Romantic",
+    liked: false,
+    singer: "Aditya Rikhari"
+  },
+  {
+    id: 4,
+    name: "Zaroor",
+    song: song4,
+    image: image4,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Aparshakti Khurana, Savi Kahlon"
+  },
+  {
+    id: 5,
+    name: "Daryaa",
+    song: song5,
+    image: image5,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Ammy Virk & Shahid Mallya"
+  },
+  {
+    id: 6,
+    name: "Pehli Nazar Mein",
+    song: song6,
+    image: image6,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Aatif Aslam"
+  },
+  {
+    id: 7,
+    name: "Tera Hone Laga Hoon",
+    song: song7,
+    image: image7,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Atif Aslam, Alisha Chinai"
+  },
+  {
+    id: 8,
+    name: "Jaanam",
+    song: song8,
+    image: image8,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Vishal Mishra"
+  },
+  {
+    id: 9,
+    name: "DIL NU",
+    song: song9,
+    image: image9,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "AP DHILLON, SHINDA KAHLON"
+  },
+  {
+    id: 10,
+    name: "Chuttamalle",
+    song: song10,
+    image: image10,
+    category: "Bollywood",
+    liked: false,
+    singer: "Anirudh Ravichander  Shilpa Rao"
+  },
+  {
+    id: 11,
+    name: "Azul",
+    song: song11,
+    image: image11,
+    category: "Romantic, Bollywood",
+    liked: false,
+    singer: "Guru Randhawa"
+  },
+  {
+    id: 12,
+    name: "Dhun",
+    song: song12,
+    image: image12,
+    category: "Bollywood",
+    liked: false,
+    singer: "Arijit Singh"
+  },
+  {
+    id: 13,
+    name: "Hass Hass",
+    song: song13,
+    image: image13,
+    category: "Bollywood",
+    liked: false,
+    singer: "Diljit X Sia"
+  },
+  {
+    id: 14,
+    name: "WAVY",
+    song: song14,
+    image: image14,
+    category: "Bollywood",
+    liked: false,
+    singer: "Karan Aujla's"
+  },
+  {
+    id: 15,
+    name: "AFSOS",
+    song: song15,
+    image: image15,
+    category: "Bollywood",
+    liked: false,
+    singer: "Aniv Jain ft.AP Dhillon"
+  },
+  {
+    id: 16,
+    name: "Jo Tum Mere Ho",
+    song: song16,
+    image: image16,
+    category: "Bollywood",
+    liked: false,
+    singer: "Anuv Jain"
+  },
+  {
+    id: 17,
+    name: "GUL",
+    song: song17,
+    image: image17,
+    category: "Bollywood",
+    liked: false,
+    singer: "Anuv Jain"
+  },
+  {
+    id: 18,
+    name: "HUSN",
+    song: song18,
+    image: image18,
+    category: "Bollywood",
+    liked: false,
+    singer: "Anuv Jain"
+  },
+  {
+    id: 19,
+    name: "Baarishein",
+    song: song19,
+    image: image19,
+    category: "Bollywood",
+    liked: false,
+    singer: "Anuv Jain"
+  },
+  {
+    id: 20,
+    name: "Labon Ko",
+    song: song20,
+    image: image20,
+    category: "Bollywood",
+    liked: false,
+    singer: "K.K"
+  },
+  {
+    id: 21,
+    name: "Dil Ibaadaat",
+    song: song21,
+    image: image21,
+    category: "Bollywood",
+    liked: false,
+    singer: "KK"
+  },
+  {
+    id: 22,
+    name: "Cold",
+    song: song22,
+    image: image22,
+    category: "Hollywood",
+    liked: false,
+    singer: "Marroon 5"
+  },
+  {
+    id: 23,
+    name: "F1-Lose My Mind",
+    song: song23,
+    image: image23,
+    category: "Hollywood",
+    liked: false,
+    singer: "Don Toliver ft.Doja Cat"
+  },
+  {
+    id: 24,
+    name: "Bye Bye Bye",
+    song: song24,
+    image: image24,
+    category: "Hollywood",
+    liked: false,
+    singer: "SYNC"
+  },
+  {
+    id: 25,
+    name: "On & On",
+    song: song25,
+    image: image25,
+    category: "Hollywood",
+    liked: false,
+    singer: "Cartoon, Jéja"
+  },
+  {
+    id: 26,
+    name: "Sofcore",
+    song: song26,
+    image: image26,
+    category: "Hollywood",
+    liked: false,
+    singer: "The Neighbourhood"
+  },
+  {
+    id: 27,
+    name: "Reflections",
+    song: song27,
+    image: image27,
+    category: "Hollywood",
+    liked: false,
+    singer: "The Neighbourhood"
+  },
+  {
+    id: 28,
+    name: "Attention",
+    song: song28,
+    image: image28,
+    category: "Hollywood",
+    liked: false,
+    singer: "Charlie Puth"
+  },
+  {
+    id: 29,
+    name: "Does It Feel",
+    song: song29,
+    image: image29,
+    category: "Hollywood",
+    liked: false,
+    singer: "Charlie Puth"
+  },
+  {
+    id: 30,
+    name: "Judas",
+    song: song30,
+    image: image30,
+    category: "Hollywood",
+    liked: false,
+    singer: "Gojo vs Sukuna(JJK) ft.Lady Gaga"
+  },
+  {
+    id: 31,
+    name: "Ghost",
+    song: song31,
+    image: image31,
+    category: "Hollywood",
+    liked: false,
+    singer: "Justin Bieber"
+  },
+  
+
+];
